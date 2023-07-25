37fbe09174604dd595c0a59a5bdbf11662cc26ec
dell <zynab5783@gmail.com>
2023-07-25 15:47:16 +0300

    The First Project

---
+++ app/src/main/java/com/example/onsiteproject/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.onsiteproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var clculateBtn: Button
+    lateinit var numberOne: EditText
+    lateinit var numberTwo: EditText
+    lateinit var result: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clculateBtn = findViewById(R.id.clculate_btn)
+        numberOne = findViewById(R.id.first_number)
+        numberTwo = findViewById(R.id.second_number)
+        result = findViewById(R.id.result_txt)
+
+
+        clculateBtn.setOnClickListener {
+            val numbOne: Int =
+            kotlin.runCatching { numberOne.text.toString().toInt() }.getOrElse { 0 }
+            val numbTne: Int =
+            kotlin.runCatching { numberTwo.text.toString().toInt() }.getOrElse { 0 }
+
+            result.text = "${numbOne + numbTne}"
+        }
+
+    }
+
+
+}
